b37df29b05000bdabfacfdb111cb224785dde8a9
Tobias Siqueira <siqueira.tobias26@gmail.com>
2025-01-20 21:50:24 -0300

    Melhoria da classe Vacina.

--- src/Vacina.java
+++ src/Vacina.java
@@ -1,6 +1,16 @@
 public class Vacina {
+
     private String substancia;
-    private String quantidade;
+    private double quantidade;
+
+    public Vacina(){
+
+    }
+
+    public Vacina(String substancia, double quantidade){
+        this.substancia = substancia;
+        this.quantidade = quantidade;
+    }
 
     public String getSubstancia() {
         return substancia;
@@ -8,10 +18,15 @@ public class Vacina {
     public void setSubstancia(String substancia) {
         this.substancia = substancia;
     }
-    public String getQuantidade() {
+    public double getQuantidade() {
         return quantidade;
     }
-    public void setQuantidade(String quantidade) {
+    public void setQuantidade(double quantidade) {
         this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString(){
+        return "Esta vacina possui a substânica: " + this.substancia + " e a quantidade: " + this.quantidade;
     }
 }
